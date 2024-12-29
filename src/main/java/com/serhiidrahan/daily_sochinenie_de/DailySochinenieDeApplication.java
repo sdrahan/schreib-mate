bcbdccff1ba5639caf2a018139d8250ba1f9ce13
sdrahan <serhii.drahan@raisin.com>
2024-12-29 17:32:47 +0100

    Initial commit

---
+++ src/main/java/com/serhiidrahan/daily_sochinenie_de/DailySochinenieDeApplication.java
@@ -0,0 +1,13 @@
+package com.serhiidrahan.daily_sochinenie_de;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailySochinenieDeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailySochinenieDeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/serhiidrahan/daily_sochinenie_de/DailySochinenieDeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serhiidrahan.daily_sochinenie_de;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailySochinenieDeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
